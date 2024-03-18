245112983396ef3e65f1fffbbcde6b1fb2a07c9c
Martinet <remymartinet.contact@gmail.com>
2024-03-18 11:12:20 +0100

    Add Dark mode functionality Add logo and application name Update page.tsx to include username with Clerk

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { Logo } from "./_components/logo";
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex flex-col items-center justify-center h-full">
+    <div className=" h-full flex flex-col items-center justify-center space-y-6">
+      <Logo />
       {children}
     </div>
   );
