36f8872309dcfb4a1ee2bbd15a592ee79583a6fa
Martinet <remymartinet.contact@gmail.com>
2024-03-27 11:06:20 +0100

    Add block and unblock actions

---
+++ actions/block.ts
@@ -0,0 +1,32 @@
+import { blockUser, unblockUser } from "@/lib/block-service";
+import { revalidatePath } from "next/cache";
+
+export const onBlock = async (id: string) => {
+  /*
+  
+  TODO: Adapt to disconnect from livestream when blocking a user
+  TODO: Allow ability to kick a user from a livestream when blocking them
+  
+  */
+  const blockedUser = await blockUser(id);
+
+  revalidatePath("/");
+
+  if (blockedUser) {
+    revalidatePath(`/${blockedUser.blocked.username}`);
+  }
+
+  return blockUser;
+};
+
+export const onUnBlock = async (id: string) => {
+  const unblockedUser = await unblockUser(id);
+
+  revalidatePath("/");
+
+  if (unblockedUser) {
+    revalidatePath(`/${unblockedUser.blocked.username}`);
+  }
+
+  return unblockUser;
+};
