7d2780560927dee91106e5de584a98445eb73a02
Martinet <remymartinet.contact@gmail.com>
2024-04-02 11:38:19 +0200

    Add new pages and components for error handling and not found pages

--- app/(browse)/[username]/page.tsx
+++ app/(browse)/[username]/page.tsx
@@ -1,8 +1,9 @@
+import { notFound } from "next/navigation";
+
+import { StreamPlayer } from "@/components/stream-player";
 import { isBlockedByUser } from "@/lib/block-service";
 import { isFollowingUser } from "@/lib/follow-service";
 import { getUserByUsername } from "@/lib/user-service";
-import { notFound } from "next/navigation";
-import { Actions } from "./_components/action";
 
 interface UserPageProps {
   params: {
@@ -13,24 +14,19 @@ interface UserPageProps {
 const UserPage = async ({ params }: UserPageProps) => {
   const user = await getUserByUsername(params.username);
 
-  if (!user) {
+  if (!user || !user.stream) {
     notFound();
   }
 
   const isFollowing = await isFollowingUser(user.id);
   const isBlocked = await isBlockedByUser(user.id);
 
+  if (isBlocked) {
+    notFound();
+  }
+
   return (
-    <div className="mt-40 flex flex-col gap-y-4">
-      <p>username: {user.username}</p>
-      <p>User Id: {user.id}</p>
-      <p>is following: {`${isFollowing}`}</p>
-      <Actions
-        userId={user.id}
-        isFollowing={isFollowing}
-        isBlocking={isBlocked}
-      />
-    </div>
+    <StreamPlayer user={user} stream={user.stream} isFollowing={isFollowing} />
   );
 };
 
